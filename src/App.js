741b380715db048ae00be6558c261e599255b07a
Md. Mosabbir Hossain <rehanmosabbir@gmail.com>
2023-09-15 12:17:56 +0600

    complete text-expander project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import "./index.css";
+
 export default function App() {
   return (
     <div>
@@ -36,35 +38,60 @@ export default function App() {
 }
 
 function TextExpander({
-  collapsedNumWords = 10,
-  expandButtonText = "Show more",
-  collapseButtonText = "Show less",
-  buttonColor = "#1f09cd",
-  expanded = false,
-  className,
   children,
+  className = "",
+  expanded = false,
+  collapsedNumWords = 20,
+  expandButtonText = "",
+  collapseButtonText = "",
+  buttonColor = "",
 }) {
   const [isExpanded, setIsExpanded] = useState(expanded);
 
-  const displayText = isExpanded
-    ? children
-    : children.split(" ").slice(0, collapsedNumWords).join(" ") + "...";
-
-  const buttonStyle = {
-    background: "none",
-    border: "none",
-    font: "inherit",
-    cursor: "pointer",
-    marginLeft: "6px",
-    color: buttonColor,
-  };
-
+  function handleClick() {
+    setIsExpanded((isExpanded) => !isExpanded);
+  }
   return (
     <div className={className}>
-      <span>{displayText}</span>
-      <button onClick={() => setIsExpanded((exp) => !exp)} style={buttonStyle}>
-        {isExpanded ? collapseButtonText : expandButtonText}
-      </button>
+      {isExpanded ? (
+        <>
+          {children}
+          {isExpanded ? (
+            <span
+              onClick={handleClick}
+              style={buttonColor ? { color: buttonColor } : { color: "blue" }}
+            >
+              {collapseButtonText === "" ? "Show less" : "Collapse Text"}
+            </span>
+          ) : (
+            <span
+              onClick={handleClick}
+              style={buttonColor ? { color: buttonColor } : { color: "blue" }}
+            >
+              ...{expandButtonText === "" ? "Show more" : expandButtonText}
+            </span>
+          )}
+        </>
+      ) : (
+        <>
+          {children.split(" ").slice(0, collapsedNumWords).join(" ")}
+          {isExpanded ? (
+            <span
+              onClick={handleClick}
+              style={buttonColor ? { color: buttonColor } : { color: "blue" }}
+            >
+              {collapseButtonText}
+            </span>
+          ) : (
+            <span
+              onClick={handleClick}
+              style={buttonColor ? { color: buttonColor } : { color: "blue" }}
+            >
+              ...{expandButtonText === "" ? "Show more" : expandButtonText}
+            </span>
+          )}
+        </>
+      )}
     </div>
   );
 }
